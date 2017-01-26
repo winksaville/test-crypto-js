b79b22d01ec787f9851a398a604a59199115e788
Wink Saville <wink@saville.com>
2017-01-26 12:09:23 -0800

    Add log.js
    A module which logs to console and optionally a textarea

---
+++ src/log.js
@@ -0,0 +1,19 @@
+/**
+ * Logging funtion always calls console.log
+ * and an option 'log_textarea'.
+ */
+var log_textarea;
+var log_textarea_id = "log_textarea";
+
+window.onload = function() {
+  log_textarea = document.getElementById(log_textarea_id);
+  log("window.onload log.js");
+}
+
+export default function log(_text) {
+  if (log_textarea != undefined) {
+    log_textarea.value += _text + "\n";
+    log_textarea.scrollTop = log_textarea.scrollHeight;
+  }
+  console.log(_text);
+}
